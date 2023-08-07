87bda474062966c19897516799a36eebf1748587
mdfaisalahmed025 <md.faisal3@northsouth.edu>
2023-08-07 14:42:43 +0600

    lol

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { User } from './user/entities/user.entity';
-import { S3Module } from './s3/s3.module';
-import { TravellerModule } from './traveller/traveller.module';
-import { Traveller } from './traveller/entities/traveller.entity';
-import { DepositModule } from './deposit/deposit.module';
-import { Deposit } from './deposit/entities/deposit.entity';
-import { AmarpayModule } from './amarpay/amarpay.module';
-import { SslcommerzModule } from './sslcommerz/sslcommerz.module';
-import { LedgerModule } from './ledger/ledger.module';
-import { GeneralLedger } from './ledger/entity';
 
 @Module({
   imports: [ConfigModule.forRoot({isGlobal: true,}),
@@ -28,17 +17,10 @@ import { GeneralLedger } from './ledger/entity';
       // host: '127.0.0.1',
       // database:'quickticketsb2c',
       port:3306,
-      entities: [User,Traveller, Deposit,GeneralLedger],
+      entities: [],
       synchronize:false,
       
     }),
-    UserModule,
-    S3Module,
-    TravellerModule,
-    DepositModule,
-    AmarpayModule,
-    SslcommerzModule,
-    LedgerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
